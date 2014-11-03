c565703ae636e0ca12af7bd1b1525a96ce5e891d
karelmikie3 <karelkling1@gmail.com>
2014-11-03 17:25:28 +0100

    making god more invincible

--- src/main/java/HxCKDMS/HxCCore/Events/EventGod.java
+++ src/main/java/HxCKDMS/HxCCore/Events/EventGod.java
@@ -6,12 +6,14 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
 import java.io.File;
 import java.util.EventListener;
 
 public class EventGod implements EventListener {
+    private int HealTimer = 0;
 
     @SubscribeEvent(receiveCanceled = true)
     public void playerHurt(LivingHurtEvent event){
@@ -57,6 +59,32 @@ public class EventGod implements EventListener {
                     exception.printStackTrace();
                 }
             }
+        }
+    }
+
+    @SubscribeEvent(receiveCanceled = true)
+    public void playerUpdate(LivingEvent.LivingUpdateEvent event){
+        if(event.entity instanceof EntityPlayer && HealTimer >= 20){
+            EntityPlayer player = ((EntityPlayer) event.entity);
+            String UUID = player.getUniqueID().toString();
+            File CustomPlayerData = new File(HxCCore.HxCCoreDir, UUID + ".dat");
+            try {
+                NBTTagCompound playerData = CompressedStreamTools.read(CustomPlayerData);
+                if (playerData.getBoolean("god") && event.isCancelable()) {
+                    player.heal(player.getMaxHealth() - player.getHealth());
+                }
+            } catch (Exception e) {
+                try {
+                    NBTTagCompound playerData = CompressedStreamTools.read(CustomPlayerData);
+                    playerData.setBoolean("god", false);
+                }catch(Exception exception){
+                    exception.printStackTrace();
+                }
+            }
+        }
+        HealTimer++;
+        if(HealTimer >= 20){
+            HealTimer = 0;
         }
     }
 }
