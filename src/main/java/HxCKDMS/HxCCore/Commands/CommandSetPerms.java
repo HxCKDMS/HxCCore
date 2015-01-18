0d044333303438b78c7a8f73cf669e239e86ca00
DrZed <kyahne12@yahoo.com>
2015-01-17 18:32:08 -0600

    setperms

--- src/main/java/HxCKDMS/HxCCore/Commands/CommandSetPerms.java
+++ src/main/java/HxCKDMS/HxCCore/Commands/CommandSetPerms.java
@@ -31,55 +31,59 @@ public class CommandSetPerms implements ISubCommand {
 
     @Override
     public void handleCommand(ICommandSender sender, String[] args) {
-        EntityPlayerMP player = (EntityPlayerMP)sender;
+        if(sender instanceof EntityPlayerMP){
+            EntityPlayerMP player = (EntityPlayerMP)sender;
 
-        File CustomPlayerData = new File(HxCCore.HxCCoreDir, "HxC-Permissions.dat");
+            File CustomPlayerData = new File(HxCCore.HxCCoreDir, "HxC-Permissions.dat");
 
-        NBTTagCompound Permissions = NBTFileIO.getNbtTagCompound(CustomPlayerData, "Permissions");
-        NBTTagCompound PermLevel0 = Permissions.getCompoundTag("PermLevel0");
-        NBTTagCompound PermLevel1 = Permissions.getCompoundTag("PermLevel1");
-        NBTTagCompound PermLevel2 = Permissions.getCompoundTag("PermLevel2");
-        NBTTagCompound PermLevel3 = Permissions.getCompoundTag("PermLevel3");
-        NBTTagCompound PermLevel4 = Permissions.getCompoundTag("PermLevel4");
-        NBTTagCompound PermLevel5 = Permissions.getCompoundTag("PermLevel5");
-        if (args.length == 3) {
-            playerName = args[1];
-            permLevel = Integer.parseInt(args[2]);
+            NBTTagCompound Permissions = NBTFileIO.getNbtTagCompound(CustomPlayerData, "Permissions");
+            NBTTagCompound PermLevel0 = Permissions.getCompoundTag("PermLevel0");
+            NBTTagCompound PermLevel1 = Permissions.getCompoundTag("PermLevel1");
+            NBTTagCompound PermLevel2 = Permissions.getCompoundTag("PermLevel2");
+            NBTTagCompound PermLevel3 = Permissions.getCompoundTag("PermLevel3");
+            NBTTagCompound PermLevel4 = Permissions.getCompoundTag("PermLevel4");
+            NBTTagCompound PermLevel5 = Permissions.getCompoundTag("PermLevel5");
+            if (args.length == 3) {
+                playerName = args[1];
+                permLevel = Integer.parseInt(args[2]);
+            }else{
+                player.addChatMessage(new ChatComponentText("\u00A74Correct usage is: " + "\u00A72/HxCCore setPerms <Player> <PermLevel(0-5)>"));
+            }
+            if (permLevel > 5 || permLevel < 0) {
+                player.addChatMessage(new ChatComponentText("\u00A74Correct usage is: " + "\u00A72/HxCCore setPerms <Player> <PermLevel(0-5)>"));
+            }
+
+            switch(permLevel){
+                case 1:
+                    PermLevel1.setInteger(playerName, permLevel);
+                    player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL1 + CC + "6."));
+                    break;
+                case 2:
+                    PermLevel2.setInteger(playerName, permLevel);
+                    player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL2 + CC + "6."));
+                    break;
+                case 3:
+                    PermLevel3.setInteger(playerName, permLevel);
+                    player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL3 + CC + "6."));
+                    break;
+                case 4:
+                    PermLevel4.setInteger(playerName, permLevel);
+                    player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL4 + CC + "6."));
+                    break;
+                case 5:
+                    PermLevel5.setInteger(playerName, permLevel);
+                    player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL5 + CC + "6."));
+                    break;
+                default:
+                    PermLevel0.setInteger(playerName, permLevel);
+                    player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL0 + CC + "6."));
+                    break;
+            }
+
+            NBTFileIO.setNbtTagCompound(CustomPlayerData, "Permissions", Permissions);
         }else{
-            player.addChatMessage(new ChatComponentText("\u00A74Correct usage is: " + "\u00A72/HxCCore setPerms <Player> <PermLevel(0-5)>"));
+            sender.addChatMessage(new ChatComponentText("\u00A74This command can only be executed by a player."));
         }
-        if (permLevel > 5 || permLevel < 0) {
-            player.addChatMessage(new ChatComponentText("\u00A74Correct usage is: " + "\u00A72/HxCCore setPerms <Player> <PermLevel(0-5)>"));
-        }
-
-        switch(permLevel){
-            case 1:
-                PermLevel1.setInteger(playerName, permLevel);
-                player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL1 + CC + "6."));
-                break;
-            case 2:
-                PermLevel2.setInteger(playerName, permLevel);
-                player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL2 + CC + "6."));
-                break;
-            case 3:
-                PermLevel3.setInteger(playerName, permLevel);
-                player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL3 + CC + "6."));
-                break;
-            case 4:
-                PermLevel4.setInteger(playerName, permLevel);
-                player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL4 + CC + "6."));
-                break;
-            case 5:
-                PermLevel5.setInteger(playerName, permLevel);
-                player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL5 + CC + "6."));
-                break;
-            default:
-                PermLevel0.setInteger(playerName, permLevel);
-                player.addChatMessage(new ChatComponentText(CC + "6" + playerName + "'s" + CC + "6 Permissions Level was set to " + PL0 + CC + "6."));
-                break;
-        }
-
-        NBTFileIO.setNbtTagCompound(CustomPlayerData, "Permissions", Permissions);
     }
 
     @Override
