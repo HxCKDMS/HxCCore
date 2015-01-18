7098d08c12361ef2242eb46602fab2f388178b45
DrZed <kyahne12@yahoo.com>
2015-01-17 18:18:28 -0600

    Command Color Update

--- src/main/java/HxCKDMS/HxCCore/Commands/CommandColor.java
+++ src/main/java/HxCKDMS/HxCCore/Commands/CommandColor.java
@@ -4,6 +4,7 @@ import HxCKDMS.HxCCore.HxCCore;
 import HxCKDMS.HxCCore.network.MessageColor;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,13 +19,16 @@ public class CommandColor implements ISubCommand {
 
     @Override
     public void handleCommand(ICommandSender sender, String[] args) {
-        EntityPlayerMP player = (EntityPlayerMP) sender;
-        String UUID = player.getUniqueID().toString();
-        
-        char color = args.length == 1 ? 'f' : args[1].toCharArray()[0];
-        if ((color < 'a' || color > 'f') && (color < '0' || color > '9')) color = 'f';
-        // Will be buggy if LAN network has people logging on/off (names will lose their colors)
-        HxCCore.network.sendToServer(new MessageColor.Message(UUID, color));
+        if(sender instanceof EntityPlayerMP){
+            EntityPlayerMP player = (EntityPlayerMP) sender;
+            String UUID = player.getUniqueID().toString();
+            char color = args.length == 1 ? 'f' : args[1].toCharArray()[0];
+            if ((color < 'a' || color > 'f') && (color < '0' || color > '9')) color = 'f';
+            // Will be buggy if LAN network has people logging on/off (names will lose their colors)
+            HxCCore.network.sendToServer(new MessageColor.Message(UUID, color));
+        }else{
+            sender.addChatMessage(new ChatComponentText("\u00A74This command can only be executed by a player."));
+        }
     }
     
     @Override
