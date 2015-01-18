414d831e9df839bc3ba85d91aa96d772047262e6
karelmikie3 <karelkling1@gmail.com>
2015-01-18 05:13:59 +0100

    added more information to the playerInfo command

--- src/main/java/HxCKDMS/HxCCore/Commands/CommandPlayerInfo.java
+++ src/main/java/HxCKDMS/HxCCore/Commands/CommandPlayerInfo.java
@@ -20,7 +20,7 @@ public class CommandPlayerInfo implements ISubCommand {
     public void handleCommand(ICommandSender sender, String[] args) {
         EntityPlayerMP player2 = net.minecraft.command.CommandBase.getPlayer(sender, args[1]);
 
-        sender.addChatMessage(new ChatComponentText(String.format("Player: %1$s is at x: %2$d, y: %3$d, z: %4$d in dimension: %5$d with gamemode: %6$s.", player2.getCommandSenderName(), (int)player2.posX, (int)player2.posY, (int)player2.posZ, player2.dimension, player2.theItemInWorldManager.getGameType().getName())));
+        sender.addChatMessage(new ChatComponentText(String.format("Player: %1$s is at x: %2$d, y: %3$d, z: %4$d in dimension: %5$d with gamemode: %6$s and connected from ip: %7$s with a ping of: %8$s.", player2.getCommandSenderName(), (int)player2.posX, (int)player2.posY, (int)player2.posZ, player2.dimension, player2.theItemInWorldManager.getGameType().getName(), player2.getPlayerIP(), player2.ping)));
     }
 
     @Override
