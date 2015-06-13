9f72dd3d4d1aaee7beb0a1ac1084bd54b0acae21
DrZed <kyahne12@yahoo.com>
2015-06-13 11:00:01 -0400

    fixed

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-gradle build dev --stacktrace
+gradle build dev --stacktrace --debug > buildlog.log
--- src/main/java/HxCKDMS/HxCCore/lib/References.java
+++ src/main/java/HxCKDMS/HxCCore/lib/References.java
@@ -4,6 +4,6 @@ public class References {
     public static final String MOD_ID = "HxCCore";
     public static final String MOD_NAME = "HxCKDMS Core";
     public static final String VERSION = "1.5.0";
-    public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448)";
+    public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,)";
     public static final String PACKET_CHANNEL_NAME = MOD_ID.toLowerCase();
 }
