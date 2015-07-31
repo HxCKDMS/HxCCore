92f0b749e37a46110e76b42d2e41f435a076db5d
DrZed <kyahne14@gmail.com>
2015-07-31 14:50:34 -0400

    Version bump

--- src/main/java/HxCKDMS/HxCCore/lib/References.java
+++ src/main/java/HxCKDMS/HxCCore/lib/References.java
@@ -3,7 +3,7 @@ package HxCKDMS.HxCCore.lib;
 public class References {
     public static final String MOD_ID = "HxCCore";
     public static final String MOD_NAME = "HxCKDMS Core";
-    public static final String VERSION = "1.6.0";
+    public static final String VERSION = "1.7.0";
     public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,)";
     public static final String PACKET_CHANNEL_NAME = MOD_ID.toLowerCase();
 
