41586d8ad500c0a697f2e0ea3668aa73bc6cbcad
DrZed <kyahne14@gmail.com>
2015-08-09 20:56:55 -0400

    version bump and copy pasta code from karel's fix in 1.7

--- src/main/java/HxCKDMS/HxCCore/Crash/CrashReportThread.java
+++ src/main/java/HxCKDMS/HxCCore/Crash/CrashReportThread.java
@@ -4,12 +4,13 @@ import HxCKDMS.HxCCore.Configs.Configurations;
 import HxCKDMS.HxCCore.HxCCore;
 import HxCKDMS.HxCCore.lib.References;
 import com.google.gson.Gson;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class CrashReportThread extends Thread {
@@ -58,11 +59,16 @@ public class CrashReportThread extends Thread {
     }
 
     private void sendToServer(ArrayList<String> crash) throws IOException {
-        Socket socket = new Socket(InetAddress.getByName(References.ERROR_REPORT_ADDRESS), References.ERROR_REPORT_PORT);
+        Socket socket = new Socket(InetAddress.getByName(getServerIP()), getServerPort());
         PrintWriter writer = new PrintWriter(socket.getOutputStream());
 
         String json = gson.toJson(new crashSendTemplate(crash));
 
+        try {
+            sleep(100L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         writer.write(json);
         writer.flush();
         writer.close();
@@ -81,6 +87,28 @@ public class CrashReportThread extends Thread {
 
         public crashSendTemplate(ArrayList<String> crash) {
             this.crash = crash;
+        }
+    }
+
+    private String getServerIP() throws IOException {
+        URL url = new URL("https://raw.githubusercontent.com/HxCKDMS/HxCLib/master/HxCIssueIP.txt");
+        InputStream inputStream = url.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String ip = reader.readLine().split(":")[0];
+        if(ip.equals("")) return References.ERROR_REPORT_ADDRESS;
+        return ip;
+    }
+
+    private int getServerPort() throws IOException {
+        URL url = new URL("https://raw.githubusercontent.com/HxCKDMS/HxCLib/master/HxCIssueIP.txt");
+        InputStream inputStream = url.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        try {
+            return Integer.parseInt(reader.readLine().split(":")[1]);
+        } catch (Exception ignored) {
+            return References.ERROR_REPORT_PORT;
         }
     }
 }
