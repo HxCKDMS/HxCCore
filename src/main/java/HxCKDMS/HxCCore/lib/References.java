7bd6879e14dc1a51a9484a55cd31208562ff9555
karelmikie3 <fl8@kpnmail.nl>
2015-08-10 02:53:21 +0200

    version bump

--- src/main/java/HxCKDMS/HxCCore/lib/References.java
+++ src/main/java/HxCKDMS/HxCCore/lib/References.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class References {
     public static final String MOD_ID = "HxCCore";
     public static final String MOD_NAME = "HxCKDMS Core";
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.8.1";
     public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,)";
     public static final String PACKET_CHANNEL_NAME = MOD_ID.toLowerCase();
 
