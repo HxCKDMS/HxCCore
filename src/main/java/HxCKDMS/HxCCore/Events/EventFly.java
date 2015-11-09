aebda9a50c79bcb18b70de1f5d427b6fa8fe1430
DrZed <kyahne14@gmail.com>
2015-11-08 21:26:06 -0500

    Forgot 2 files it was pushed to curse though

---
+++ src/main/java/HxCKDMS/HxCCore/Events/EventFly.java
@@ -0,0 +1,28 @@
+package HxCKDMS.HxCCore.Events;
+
+import HxCKDMS.HxCCore.Handlers.NBTFileIO;
+import HxCKDMS.HxCCore.HxCCore;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.living.LivingEvent;
+
+import java.io.File;
+
+public class EventFly {
+    private int timer = 20;
+    @SubscribeEvent
+    public void eventUpdate(LivingEvent.LivingUpdateEvent event) {
+        if (event.entityLiving instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer)event.entityLiving;
+            String UUID = player.getUniqueID().toString();
+            File CustomPlayerData = new File(HxCCore.HxCCoreDir, "HxC-" + UUID + ".dat");
+            timer--;
+            if(!CustomPlayerData.exists()) return;
+            if (timer <= 0 && NBTFileIO.getBoolean(CustomPlayerData, "fly")) {
+                timer = 600;
+                player.capabilities.allowFlying = true;
+                player.sendPlayerAbilities();
+            }
+        }
+    }
+}
