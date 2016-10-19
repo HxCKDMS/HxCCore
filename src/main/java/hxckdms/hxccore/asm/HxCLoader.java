33c5e6a5e3b2510df878a4f4060e5d89fbae9b9d
Sietse <fl8@kpnmail.nl>
2016-10-19 13:40:44 +0200

    doesn't work on linux apparently.

--- src/main/java/hxckdms/hxccore/asm/HxCLoader.java
+++ src/main/java/hxckdms/hxccore/asm/HxCLoader.java
@@ -31,16 +31,11 @@ public class HxCLoader implements IFMLLoadingPlugin {
     public void injectData(Map<String, Object> data) {
         RuntimeDeobf = (Boolean) data.get("runtimeDeobfuscationEnabled");
 
-        String arch = System.getenv("PROCESSOR_ARCHITECTURE");
-        String wow64Arch = System.getenv("PROCESSOR_ARCHITEW6432");
-
-        String realArch = arch.endsWith("64") || wow64Arch != null && wow64Arch.endsWith("64") ? "64" : "32";
-
         System.out.println(System.getProperty("java.version"));
 
         try {
             int major = Integer.parseInt(System.getProperty("java.version").split("\\.")[1]);
-            if (major < 8) throw new RuntimeException("Old java version detected, please download the latest " + realArch + "-bit version of java from http://java.com/en/download/manual.jsp");
+            if (major < 8) throw new RuntimeException("Old java version detected, please download the latest version of java from http://java.com/en/download/manual.jsp");
         } catch (NumberFormatException ignored) {}
     }
 
